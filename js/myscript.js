858bf9dc36ed2da10479f53f6d34d70d76be2a1b
Luna Lopis <lunalopis59@gmail.com>
2023-05-30 16:17:11 +0200

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,42 @@
+function createSquareElement() {
+    let square = document.createElement('div');
+    square.classList.add('square');
+    return square;
+  }
+  
+  function generateGrid() {
+    const gridSizeSelect = document.getElementById('gridSizeSelect');
+    const gridSize = parseInt(gridSizeSelect.value);
+  
+    const grid = document.getElementById('grid');
+    grid.innerHTML = '';
+    grid.className = 'grid-' + gridSize;
+  
+    // inserisco array 16 random numeri dentro la funzione genera per generare l'array contestualmente alla griglia selezionata.
+    const randomNumbers = [];
+    while (randomNumbers.length < 16) {
+        const randomNumber = Math.floor(Math.random() * gridSize) + 1;
+        if (!randomNumbers.includes(randomNumber)) {
+            randomNumbers.push(randomNumber);
+        }
+    }
+  
+    for (let i = 0; i < gridSize; i++) {
+        let square = createSquareElement();
+        grid.append(square);
+        square.innerText = i + 1;
+  
+        square.addEventListener('click', function () {
+            const squareNumber = i + 1;
+            if (randomNumbers.includes(squareNumber)) {
+                this.style.backgroundColor = 'red';
+                square.removeEventListener('click');
+            } else {
+                this.classList.add('clicked');
+            }
+        })
+    }
+  }
+  
+  const generateButton = document.getElementById('generateButton');
+  generateButton.addEventListener('click', generateGrid);
---
+++ readme.md
@@ -0,0 +1,2 @@
+1. creo while per generare array random dentro generate grid e la dichiaro striga vuota prima.
+2. creo condizioni dentro eventlistener('click') per random number.
